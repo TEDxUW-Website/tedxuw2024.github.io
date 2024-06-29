cb5040facac664e8dcf69edda6580394795d3c90
Iris <irismo1009@gmail.com>
2024-06-29 18:11:10 -0400

    lottie animation kinda working
    it's bad bc lottie is stupid

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import Moon from "./Moon.svg";
-import HeaderBackground from "./Rectangle 202.svg"
+import HeaderBackground from "./Homepage1-frame1.svg"
 import TEDxLogo from "./photo_2024-06-11_10-29-54-removebg-preview 1.png"
 
 export { 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { TEDxLogo } from '../../assets'
+import Lottie, { eventListeners, options } from 'react-lottie'
+import shootingstar from './TEDx-star.json';
 
 const Header = () => {
+    const [animationFinished, setAnimationFinished] = useState(false);
+
+    const defaultOptions = {
+        loop: false,
+        autoplay: true, 
+        animationData: shootingstar,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
     return (
-        <header>
-            <div className="wrapper">
-                <div className="mainText">
-                    <div className="title">
-                        <h1>In The Odd Chance</h1>
-                    </div>
-                    <div className="subtitle">
-                        <img src={TEDxLogo} alt="TEDx Logo" className="tedxLogo"></img>
-                        <div className="info">
-                            <p>Date</p>
-                            <p>Time</p>
-                            <p>Location</p>
+        <header style={{height: '100vh', width: '100vw'}}>
+            <div className="wrapper" style={{height: '100%', width: '100%'}}>
+                {animationFinished ? (
+                    <div className="mainText">
+                        <div className="title">
+                            <h1>In The Odd Chance</h1>
                         </div>
-                    </div>
-                </div>  
+                        <div className="subtitle">
+                            <img src={TEDxLogo} alt="TEDx Logo" className="tedxLogo"></img>
+                            <div className="info">
+                                <p>Date</p>
+                                <p>Time</p>
+                                <p>Location</p>
+                            </div>
+                        </div>
+                    </div>  
+                ) : (
+                    <>
+                    <Lottie options={defaultOptions} height={'100%'} width={'100%'} 
+                    eventListeners={[
+                        {
+                            eventName: 'complete',
+                            callback: () => {
+                                console.log('the animation completed:')
+                                setAnimationFinished(true)
+                            }
+                        },
+                      ]}/>
+                    </>
+                )}
             </div>
         </header>
     )

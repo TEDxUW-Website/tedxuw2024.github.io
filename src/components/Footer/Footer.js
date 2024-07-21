f7b240e7dcad30e420a819ea60ce9ec8cdc16a50
Iris <irismo1009@gmail.com>
2024-07-21 09:08:27 -0400

    completed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,19 +12,31 @@ const Footer = () => {
                     <img src={tedx_logo_footer} alt="TEDx Logo" className="logo"></img>
                 </div>
                 <div className='timeline'>
-                    <PastEventCard year="2016" text="Interconnectedness of Things" />
-                    <PastEventCard year="2017" text="Defying Conventions" />
-                    <PastEventCard year="2018" text="Making Waves" />
-                    <PastEventCard year="2020" text="Press Play" />
+                    <PastEventCard year="2016" text="Interconnectedness of Things" link="https://2016.tedxuw.com/"/>
+                    <PastEventCard year="2017" text="Defying Conventions" link="https://2017.tedxuw.com/"/>
+                    <PastEventCard year="2018" text="Making Waves" link="https://2018.tedxuw.com/" />
+                    <PastEventCard year="2020" text="Press Play" link="https://tedxuw2.godaddysites.com/"/>
                     <PastEventCard year="2023" text="From the Ground Up"/>
                 </div>
                 <div className='socials'>
-                    <img src={youtube} alt="youtube" className="sm_icon"></img>
-                    <img src={linkedin} alt="linkedin" className="sm_icon"></img>
-                    <img src={x} alt="x" className="sm_icon"></img>
-                    <img src={ig} alt="ig" className="sm_icon"></img>
-                    <img src={fb} alt="fb" className="sm_icon"></img>
-                    <img src={mail} alt="email" className="sm_icon"></img>
+                    <a href="https://www.youtube.com/@TEDx" target="_blank" rel="noopener noreferrer">
+                        <img src={youtube} alt="youtube" className="sm_icon"></img>
+                    </a>
+                    <a href="https://www.linkedin.com/company/tedxuw/" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedin} alt="linkedin" className="sm_icon"></img>
+                    </a>
+                    <a href="https://twitter.com/TEDxUW" target="_blank" rel="noopener noreferrer">
+                        <img src={x} alt="x" className="sm_icon"></img>
+                    </a>
+                    <a href="https://www.instagram.com/tedxuw/" target="_blank" rel="noopener noreferrer">
+                        <img src={ig} alt="ig" className="sm_icon"></img>
+                    </a>
+                    <a href="https://www.facebook.com/TEDxUW/" target="_blank" rel="noopener noreferrer">
+                        <img src={fb} alt="fb" className="sm_icon"></img>
+                    </a>
+                    <a href="outreach@tedxuw.com" target="_blank" rel="noopener noreferrer">
+                        <img src={mail} alt="email" className="sm_icon"></img>
+                    </a>
                 </div>
             </div>
         </footer>
--- src/components/PastEventCard/PastEventCard.js
+++ src/components/PastEventCard/PastEventCard.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './PastEventCard.css';
 
-const PastEventCard = ({ year, text }) => {
+const getYearBackground = (year) => {
+  switch (year) {
+    case "2016":
+      return { background: 'linear-gradient(90deg, #000 0%, #4F3138 100%)' };
+    case "2017":
+      return { background: 'linear-gradient(90deg, #4F3138 0%, #70303D 100%)' };
+    case "2018":
+      return { background: 'linear-gradient(90deg, #70303D 0%, #902B3F 100%)' };
+    case "2020":
+      return { background: 'linear-gradient(90deg, #902B3F 0%, #B0233C 100%)' };
+    case "2023":
+      return { background: 'linear-gradient(90deg, #CE1534 0%, #EB0028 100%)' };
+    default:
+      return { background: 'linear-gradient(90deg, #CE1534 0%, #EB0028 100%)' };
+  }
+};
+
+const getTextBackground = (year) => {
+  switch (year) {
+    case "2016":
+      return { background: 'linear-gradient(180deg, #2A2A2A 0%, #4F3138 126.56%), #D9D9D9' };
+    case "2017":
+      return { background: 'linear-gradient(180deg, #4F3138 19.44%, #70303D 100%), #D9D9D9' };
+    case "2018":
+      return { background: 'linear-gradient(180deg, #70303D 19.44%, #902B3F 100%), #D9D9D9' };
+    case "2020":
+      return { background: 'linear-gradient(180deg, #892C3F 19.44%, #B0233C 100%), #D9D9D9' };
+    case "2023":
+      return { background: 'linear-gradient(180deg, #A9253D 19.44%, #EB0028 100%), #D9D9D9' };
+    default:
+      return { background: 'linear-gradient(180deg, #A9253D 19.44%, #EB0028 100%), #D9D9D9;' };
+  }
+};
+
+
+const PastEventCard = ({ year, text, link }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="card-container">
-      <div className="year-box">{year}</div>
-      <div className="text-box">{text}</div>
-    </div>
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <div className="card-container" style={isMobile ? getTextBackground(year) : {}}>
+        {isMobile ? (
+          <>
+            <div className="mobile-year-box">{year}</div>
+            <div className="mobile-text-box">{text}</div>
+          </>
+        ) : (
+          <>
+            <div className="year-box" style={getYearBackground(year)}>{year}</div>
+            <div className="text-box">{text}</div>
+          </>
+        )}
+      </div>
+    </a>
   );
 };
 
